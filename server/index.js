2e0d6bc2779ae3a43fa3fa77694cd69a48afea13
codeandtheory <chembeer.agaton@codeandtheory.com>
2023-07-31 06:59:55 +0800

    test

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 6000;
+const sendmail = require("sendmail")();
+
+sendmail(
+  {
+    from: "chembeeragaton@gmail.com",
+    to: "chembeeragaton@gmail.com",
+    subject: "test sendmail",
+    html: "Mail of test sendmail ",
+  },
+  function (err, reply) {
+    console.log(err && err.stack);
+    console.dir(reply);
+  }
+);
 
 app.get("/", (req, res) => {
   res.send("test2");
